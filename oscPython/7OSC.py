7d9b9c9313bb820de83d020583f8184348b4fefb
Jose Urgiles <cottonmouth@MacBook-Pro-de-Cottonmouth.local>
2015-05-25 12:44:29 -0500

    Se quitaron las librerias innecesarias para ../oscPython/7OSC.py

--- oscPython/7OSC.py
+++ oscPython/7OSC.py
@@ -3,8 +3,6 @@ This program sends 10 random values between 0.0 and 1.0 to the /filter address,
 waiting for 1 seconds between each value.
 """
 import argparse
-import random
-import time
 
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
